b37340a66f24743e1aa937e87dd811456f9ada18
ktamilara <42666475+ktamilara@users.noreply.github.com>
2019-02-22 14:13:45 +0530

    Update and rename Multi.java to Multiple.java

---
+++ Multiple.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+class Multiple
+{
+public static void main(String args[])
+{
+System.out.println("enter a number");
+Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+for(int i=1;i<=5;i++)
+{
+System.out.println(n*i);
+}
+}
